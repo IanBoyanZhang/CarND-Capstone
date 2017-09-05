2b9a08fb511a0085e7ea5054734a107642531f93
Ian Zhang <ianzhang1986@gmail.com>
2017-09-05 06:44:11 -0700

    [Feature] Propose steer PID controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -6,6 +6,12 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
+        # PID controller(s)
+        _kp_steer = args[0]
+        _ki_steer = args[1]
+        _kd_steer = args[2]
+
+        pid_steer = PID(_kp_steer, _ki_steer, _kd_steer)
         pass
 
     def control(self, *args, **kwargs):
