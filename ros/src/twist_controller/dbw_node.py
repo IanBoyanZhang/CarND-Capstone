88c6c52edac40d055a56cfa52227829450719449
Ian Zhang <ianzhang1986@gmail.com>
2017-09-30 09:27:36 -0700

    [Feature] Use false as default value

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -95,7 +95,7 @@ class DBWNode(object):
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
         rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_cb)
         rospy.Subscriber('/twist_cmd', TwistStamped, self.dbw_twist_cb)
-        if rospy.get_param('~use_dbw', True):
+        if rospy.get_param('~use_dbw', False):
             self.loop()
 
     def loop(self):
