1f2160d92668bbee1eadb402845aa58e3f6ab184
Ian Zhang <ianzhang1986@gmail.com>
2017-09-30 15:32:10 -0700

    [Feature] Log wp

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -79,6 +79,8 @@ class WaypointUpdater(object):
             n_stop_wp = len(self.velocity_map)
             for i in range(b - a):
                 rospy.logwarn('i: %s', i)
+                rospy.logwarn('a: %s', a)
+                rospy.logwarn('b: %s', b)
                 print(i)
                 v = self.velocity_map[i] if i < n_stop_wp else 0.0
                 self.set_waypoint_velocity(lane.waypoints, i, v)
